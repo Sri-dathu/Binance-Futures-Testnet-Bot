3a91b72aaa2b263f16c8e12bf4f99cf8c17f8a24
Sridattu Reddy <sridattureddypiduru@gmail.com>
2026-03-29 01:26:53 +0530

    Configure logging files

---
+++ bot/logging_config.py
@@ -0,0 +1,17 @@
+import logging
+
+def setup_logger():
+    logger = logging.getLogger("trading_bot")
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler("app.log")
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
+    )
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+    return logger
